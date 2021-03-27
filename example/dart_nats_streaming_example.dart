0bdbf752dd70ee8a69d8c24db697646878a8d815
Bruno GT <bruno@goistavares.com>
2021-03-27 14:07:32 +0000

    Example: testing connectivity

--- example/dart_nats_streaming_example.dart
+++ example/dart_nats_streaming_example.dart
@@ -1,1 +1,7 @@
-void main() {}
+import 'package:dart_nats_streaming/dart_nats_streaming.dart';
+
+void main() async {
+  final stan = Client();
+  await stan.connect(host: 'localhost');
+  print('connected: ${stan.connected}');
+}
