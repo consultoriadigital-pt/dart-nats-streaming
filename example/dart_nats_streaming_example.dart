f6bc091917c742abbfe4ee1e224ab66be1270dd2
Bruno GT <bruno@goistavares.com>
2021-03-27 15:33:52 +0000

    Example: Adjusting example with onConnect() & onDisconnect()

--- example/dart_nats_streaming_example.dart
+++ example/dart_nats_streaming_example.dart
@@ -2,6 +2,14 @@ import 'package:dart_nats_streaming/dart_nats_streaming.dart';
 
 void main() async {
   final stan = Client();
+
+  stan.onConnect(function: () {
+    print('Stan connected..');
+  });
+
+  stan.onDisconnect(function: () {
+    print('Stan disconnected..');
+  });
+
   await stan.connect(host: 'localhost');
-  print('connected: ${stan.connected}');
 }
